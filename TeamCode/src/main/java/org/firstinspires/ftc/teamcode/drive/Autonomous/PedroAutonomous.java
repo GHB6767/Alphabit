1765311055c2cdf3297686cddb086561840d283d
BigMathew <roberthajdu05@gmail.com>
2026-03-07 13:20:47 +0200

    fix(Autonomous): added the rest of the paths to the red basket auto for 9 ball

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Autonomous/PedroAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Autonomous/PedroAutonomous.java
@@ -6,7 +6,6 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.TelemetryManager;
 import com.bylazar.telemetry.PanelsTelemetry;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-import org.opencv.core.Mat;
 
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
@@ -18,7 +17,7 @@ import com.pedropathing.geometry.Pose;
 @Configurable // Panels
 public class PedroAutonomous extends OpMode {
     private Timer pathTimer, opModeTimer;
-    private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+    //private TelemetryManager panelsTelemetry; // Panels Telemetry instance
     public Follower follower; // Pedro Pathing follower instance
     public enum PathState{
         //START POSITION_END POSITION
@@ -27,52 +26,185 @@ public class PedroAutonomous extends OpMode {
 
         DRIVE_STARTPOS_SHOOT_POS,
         SHOOT_PRELOAD,
-        FIRST_ARTIFACT_LINE
+        DRIVE_FIRST_ARTEFACT_LINE,
+        OPEN_GATE,
+        ROTATE_AFTER_OPEN_GATE,
+        ROTATE_AFTER_OPEN_TO_SHOOT,
+        SHOOT_SECOND_ARTIFACT,
+        SHOOT_TO_SECOND_ARTIFACT_LINE,
+        ROTATE_AFTER_SECOND_LINE,
+        ROTATE_AFTER_SECOND_LINE_TO_SHOOT
     }
 
     PathState pathState;
 
-    private final Pose startPose = new Pose(116.920, 132.140,Math.toRadians(36.5));
-    private final Pose shootPose = new Pose(87.037, 96.993,Math.toRadians(0));
-    private final  Pose firstArtifactLine = new Pose(102.80301274623407,84.08922363847047, Math.toRadians(0));
-
-    private PathChain driveStartPosToShootPos, driveShootPosToFirstArtifactLine;
+    public  Pose startPose = new Pose(116.920,132.140,Math.toRadians(36.5));
+    public PathChain startPoseToShootPose;
+    public PathChain shootPoseToFirstArtefactLine;
+    public PathChain openGate;
+    public PathChain rotateAfterOpenGate;
+    public PathChain rotateAfterOpenToShoot;
+    public PathChain shootToSecondArtifacLine;
+    public PathChain rotateAfterSecondLine;
+    public PathChain rotateAfterSecondLineToShoot;
 
 
     public void buildPaths(){
         //put in coordonates for starting pos > put in coordonates for end pos
-        driveStartPosToShootPos = follower.pathBuilder()
-                .addPath(new BezierLine(startPose, shootPose))
-                .setLinearHeadingInterpolation(startPose.getHeading(),shootPose.getHeading())
+
+        startPoseToShootPose = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(
+                                new Pose(116.920, 132.140),
+                                new Pose(88.705, 90.396)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(36.5), Math.toRadians(0))
+                .build();
+        shootPoseToFirstArtefactLine = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(88.705, 90.396),
+                                new Pose(104.860, 80.299),
+                                new Pose(128.357, 83.882)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
                 .build();
 
-        driveShootPosToFirstArtifactLine = follower.pathBuilder()
-                .addPath(new BezierLine(shootPose, firstArtifactLine))
-                .setLinearHeadingInterpolation(shootPose.getHeading(),firstArtifactLine.getHeading())
+        openGate = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(128.357, 83.882),
+                                new Pose(115.133, 76.589),
+                                new Pose(128.535, 70.001)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
                 .build();
 
-    }//
+//        rotateAfterOpenGate = follower.pathBuilder()
+//                .addPath(
+//                        new BezierLine(
+//                                new Pose(128.535, 70.001),
+//                                new Pose(86.999, 83.819)
+//                        )
+//                )
+//                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+//                .build();
+        rotateAfterOpenToShoot = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(
+                                new Pose(128.535, 70.001),
+                                new Pose(86.999, 83.819)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                .build();
+
+        shootToSecondArtifacLine = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(86.999, 83.819),
+                                new Pose(92.831, 69.012),
+                                new Pose(89.488, 57.331),
+                                new Pose(134.322, 59.068)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                .build();
+
+//        rotateAfterSecondLine = follower.pathBuilder()
+//                .addPath(
+//                        new BezierLine(
+//                                new Pose(134.322, 59.068),
+//                                new Pose(127.449, 61.678)
+//                        )
+//                )
+//                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(336))
+//                .build();
+
+        rotateAfterSecondLineToShoot = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(
+                                new Pose(134.322, 59.068),
+                                new Pose(86.537, 80.307)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(336))
+                .build();
+    }
+
 
     public void statePathUpdate(){
         switch(pathState){
             case DRIVE_STARTPOS_SHOOT_POS:
-                follower.followPath(driveStartPosToShootPos,true);
-                setPathState(PathState.DRIVE_STARTPOS_SHOOT_POS);// reset timer and make new state
+                follower.followPath(startPoseToShootPose,false);
+                setPathState(PathState.SHOOT_PRELOAD);
                 break;
             case SHOOT_PRELOAD:
                 //TODO add shooting logic
-                //check is follower is done ?
                 if(!follower.isBusy()){
-                    telemetry.addLine("Done Path 1");
-                    setPathState(PathState.DRIVE_STARTPOS_SHOOT_POS);
-                    //transition to next state
+                    telemetry.addLine("Done Path 1: Shot Preload");
+                    setPathState(PathState.DRIVE_FIRST_ARTEFACT_LINE);
                 }
                 break;
-            case FIRST_ARTIFACT_LINE:
+            case DRIVE_FIRST_ARTEFACT_LINE:
                 if(!follower.isBusy()){
-                    follower.followPath(driveShootPosToFirstArtifactLine,true);
-                    telemetry.addLine("Done Path 2");
+                    follower.followPath(shootPoseToFirstArtefactLine,true);
+                    telemetry.addLine("Done Path 2: Driving to First Artifact");
+                    setPathState(PathState.OPEN_GATE);
                 }
+                break;
+            case OPEN_GATE:
+                if(!follower.isBusy()){
+                    follower.followPath(openGate,true);
+                    telemetry.addLine("Opening Gate");
+                    setPathState(PathState.ROTATE_AFTER_OPEN_TO_SHOOT);
+                }
+                break;
+            case ROTATE_AFTER_OPEN_GATE:
+                if(!follower.isBusy()){
+                    follower.followPath(rotateAfterOpenGate,true);
+                    telemetry.addLine("Rotating after opening gate");
+                    setPathState(PathState.ROTATE_AFTER_OPEN_TO_SHOOT);
+                }
+                break;
+            case ROTATE_AFTER_OPEN_TO_SHOOT:
+                if(!follower.isBusy()){
+                    follower.followPath(rotateAfterOpenToShoot,true);
+                    telemetry.addLine("Rotating to shoot position");
+                    setPathState(PathState.SHOOT_SECOND_ARTIFACT);
+                }
+                break;
+            case SHOOT_SECOND_ARTIFACT:
+                //TODO add shooting logic
+                if(!follower.isBusy()){
+                    telemetry.addLine("Shot Second Artifact");
+                    setPathState(PathState.SHOOT_TO_SECOND_ARTIFACT_LINE);
+                }
+                break;
+            case SHOOT_TO_SECOND_ARTIFACT_LINE:
+                if(!follower.isBusy()){
+                    follower.followPath(shootToSecondArtifacLine,true);
+                    telemetry.addLine("Driving to Second Artifact Line");
+                    setPathState(PathState.ROTATE_AFTER_SECOND_LINE_TO_SHOOT);
+                }
+                break;
+            case ROTATE_AFTER_SECOND_LINE:
+                if(!follower.isBusy()){
+                    follower.followPath(rotateAfterSecondLine,true);
+                    telemetry.addLine("Rotating after second artifact line");
+                    setPathState(PathState.ROTATE_AFTER_SECOND_LINE_TO_SHOOT);
+                }
+                break;
+            case ROTATE_AFTER_SECOND_LINE_TO_SHOOT:
+                if(!follower.isBusy()){
+                    follower.followPath(rotateAfterSecondLineToShoot,true);
+                    telemetry.addLine("Rotating to final shoot position");
+                    //TODO transition to next cycle or end
+                }
+                break;
             default:
                 telemetry.addLine("no state commanded");
                 break;
@@ -107,11 +239,11 @@ public class PedroAutonomous extends OpMode {
         follower.update();
         statePathUpdate();
 
+        telemetry.addData("Path State", pathState);
+        telemetry.addData("X", follower.getPose().getX());
+        telemetry.addData("Y", follower.getPose().getY());
+        telemetry.addData("Heading", follower.getPose().getHeading());
+        telemetry.update();
 
-        panelsTelemetry.debug("Path State", pathState);
-        panelsTelemetry.debug("X", follower.getPose().getX());
-        panelsTelemetry.debug("Y", follower.getPose().getY());
-        panelsTelemetry.debug("Heading", follower.getPose().getHeading());
-        panelsTelemetry.update(telemetry);
     }
 }
